e32c4787357a854134a9eba7e932ce5960403fa5
Renata Cummins <renatacummins@gmail.com>
2019-09-19 17:54:41 -0400

    feature: add function to add bombs to grid

--- frontend/components/minesweeper.js
+++ frontend/components/minesweeper.js
@@ -22,18 +22,47 @@ export class Tile {
 
 export class Board {
   constructor(rowCount, columnCount, bombCount) {
-    this.grid = this.constructGrid(rowCount, columnCount);
+    this.rowCount = rowCount;
+    this.columnCount = columnCount;
+    this.bombCount = bombCount;
+    this.grid = this.constructGrid();
+    this.addBombsToGrid();
   }
 
-  constructGrid(rowCount, columnCount) {
-    const grid = [...Array(rowCount)].map( element => Array(columnCount));
+  addBombsToGrid() {
+    while (this.totalBombs() < this.bombCount) {
+      let row = Math.floor(Math.random() * this.rowCount);
+      let column = Math.floor(Math.random() * this.columnCount);
 
-    for (let row = 0; row < rowCount; row++) {
-      for (let column = 0; column < columnCount; column++) {
+      if (!this.grid[row][column].has_bomb) {
+        this.grid[row][column].addBomb();
+      }
+    }
+  }
+
+  constructGrid() {
+    const grid = [...Array(this.rowCount)].map( element => (
+      Array(this.columnCount))
+    );
+
+    for (let row = 0; row < this.rowCount; row++) {
+      for (let column = 0; column < this.columnCount; column++) {
         grid[row][column] = new Tile([row, column], this);
       }
     }
 
     return grid;
+  }
+
+  totalBombs() {
+    let total = 0;
+
+    this.grid.flat().forEach( tile => {
+      if (tile.has_bomb) {
+        total += 1;
+      }
+    });
+
+    return total;
   }
 }
