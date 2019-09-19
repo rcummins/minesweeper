0e36bad7307873ce7d5787772fb0de91df73ac3f
Renata Cummins <renatacummins@gmail.com>
2019-09-19 16:56:37 -0400

    feature: add Board class with constructGrid function

--- frontend/components/minesweeper.js
+++ frontend/components/minesweeper.js
@@ -19,3 +19,21 @@ export class Tile {
     this.flag = !this.flag;
   }
 }
+
+export class Board {
+  constructor(rowCount, columnCount, bombCount) {
+    this.grid = this.constructGrid(rowCount, columnCount);
+  }
+
+  constructGrid(rowCount, columnCount) {
+    const grid = [...Array(rowCount)].map( element => Array(columnCount));
+
+    for (let row = 0; row < rowCount; row++) {
+      for (let column = 0; column < columnCount; column++) {
+        grid[row][column] = new Tile([row, column], this);
+      }
+    }
+
+    return grid;
+  }
+}
